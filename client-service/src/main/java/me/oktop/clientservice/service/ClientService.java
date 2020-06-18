fbbdacaf8e0a89d82ec60b4421b778ccc746c0a0
leehayun <hylee@goqual.com>
2020-06-18 19:50:16 +0900

    클라이언트 서버 생성

---
+++ client-service/src/main/java/me/oktop/clientservice/service/ClientService.java
@@ -0,0 +1,17 @@
+package me.oktop.clientservice.service;
+
+import me.oktop.clientservice.config.ServiceConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClientService {
+
+//    @Autowired
+//    private ServiceConfig serviceConfig;
+//
+//    public void getConfigServiceProperty() {
+//        System.out.println(serviceConfig.getExampleProperty());
+//    }
+
+}
