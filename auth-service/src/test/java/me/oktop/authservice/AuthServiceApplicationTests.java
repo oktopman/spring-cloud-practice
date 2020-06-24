a9203bf91c52670a57e9b9f776747ef33422a9ee
oktopman <colaws3@gmail.com>
2020-06-24 11:57:14 +0900

    auth-service initialize

---
+++ auth-service/src/test/java/me/oktop/authservice/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package me.oktop.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
